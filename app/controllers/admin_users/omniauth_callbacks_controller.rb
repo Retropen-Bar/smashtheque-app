b06b17ce9746ca4b2c10291e933a37c0c11bb696
Yann Hourdel <yann@hourdel.fr>
2020-08-18 14:36:25 +0200

    fix admin failure

--- app/controllers/admin_users/omniauth_callbacks_controller.rb
+++ app/controllers/admin_users/omniauth_callbacks_controller.rb
@@ -2,13 +2,12 @@ class AdminUsers::OmniauthCallbacksController < Devise::OmniauthCallbacksControl
   def discord
     admin_user = AdminUser.from_omniauth(request.env["omniauth.auth"])
 
-    if admin_user.persisted?
+    if admin_user && admin_user.persisted?
       # this will throw if admin_user is not activated
       sign_in_and_redirect admin_user, event: :authentication
       set_flash_message(:notice, :success, kind: "Discord") if is_navigational_format?
     else
-      flash.notice = "Err: " + admin_user.errors.full_messages.to_sentence
-      redirect_to new_admin_user_session_url
+      redirect_to root_path
     end
   end
 
