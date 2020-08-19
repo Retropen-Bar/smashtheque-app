5ab7339bff19e381cd6b5bfa04dd132525431ca5
Yann Hourdel <yann@hourdel.fr>
2020-08-19 16:44:38 +0200

    handle city changes

--- app/models/player.rb
+++ app/models/player.rb
@@ -32,19 +32,35 @@ class Player < ApplicationRecord
     if destroyed?
       # this is a deletion
       RetropenBot.default.rebuild_abc_for_name name
-
-    elsif previous_changes.has_key?('name')
-      # this is creation or an update with changes on the name
-      old_name = previous_changes['name'].first
-      new_name = previous_changes['name'].last
-      if RetropenBot.default.name_letter(old_name) != RetropenBot.default.name_letter(new_name)
-        RetropenBot.default.rebuild_abc_for_name old_name
-      end
-      RetropenBot.default.rebuild_abc_for_name new_name
-
+      RetropenBot.default.rebuild_cities_for_city city if city
     else
-      # this is an update, and the name didn't change
-      RetropenBot.default.rebuild_abc_for_name name
+      # name
+      if previous_changes.has_key?('name')
+        # this is creation or an update with changes on the name
+        old_name = previous_changes['name'].first
+        new_name = previous_changes['name'].last
+        if RetropenBot.default.name_letter(old_name) != RetropenBot.default.name_letter(new_name)
+          RetropenBot.default.rebuild_abc_for_name old_name
+        end
+        RetropenBot.default.rebuild_abc_for_name new_name
+
+      else
+        # this is an update, and the name didn't change
+        RetropenBot.default.rebuild_abc_for_name name
+      end
+
+      # city
+      if previous_changes.has_key?('city_id')
+        # this is creation or an update with changes on the city_id
+        old_city_id = previous_changes['city_id'].first
+        new_city_id = previous_changes['city_id'].last
+        RetropenBot.default.rebuild_cities_for_city City.find(old_city_id) if old_city_id
+        RetropenBot.default.rebuild_cities_for_city City.find(new_city_id)
+
+      else
+        # this is an update, and the city_id didn't change
+        RetropenBot.default.rebuild_cities_for_city city
+      end
     end
 
     # this handles both existing and new characters
