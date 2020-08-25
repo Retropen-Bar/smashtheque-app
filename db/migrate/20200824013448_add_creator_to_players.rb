bf7034720224ece29c704bec856a1399dbcfada3
Yann Hourdel <yann@hourdel.fr>
2020-08-25 10:51:32 +0200

    fix migration

--- db/migrate/20200824013448_add_creator_to_players.rb
+++ db/migrate/20200824013448_add_creator_to_players.rb
@@ -1,6 +1,6 @@
 class AddCreatorToPlayers < ActiveRecord::Migration[6.0]
   def change
     add_belongs_to :players, :creator, index: true
-    Player.update_all creator_id: DiscordUser.first.id
+    Player.update_all creator_id: DiscordUser.first&.id
   end
 end
