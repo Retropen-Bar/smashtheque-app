0553729de841e146aea2a0b88c06d6e5700ec527
Yann Hourdel <yann@hourdel.fr>
2020-08-28 18:27:16 +0200

    fix missing API scopes

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::CharactersController < Api::V1::BaseController
 
+  has_scope :by_emoji
+
   def index
-    characters = Character.all
+    characters = apply_scopes(Character).all
     render json: characters
   end
 
--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::CitiesController < Api::V1::BaseController
   has_scope :page, default: 1
   has_scope :per
   has_scope :on_abc
+  has_scope :by_name_like
 
   def index
     cities = apply_scopes(City).all
