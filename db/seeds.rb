3d09515ad3fbd978d49fa85cdc3319b75ccd7384
Yann Hourdel <yann@hourdel.fr>
2020-08-30 23:11:10 +0200

    fix new seeding #2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,49 +7,57 @@
 ENV['NO_DISCORD'] = '1'
 
 # City
-uri = "#{ENV['SEED_DATA_SOURCE_URL']}cities.json"
-puts "seed #{uri}"
-open(uri) do |file|
-  cities = JSON.parse(file.read)
-  cities.each do |city|
-    City.create!(city)
+unless City.any?
+  uri = "#{ENV['SEED_DATA_SOURCE_URL']}cities.json"
+  puts "seed #{uri}"
+  open(uri) do |file|
+    cities = JSON.parse(file.read)
+    cities.each do |city|
+      City.create!(city)
+    end
   end
 end
 
 # Character
-uri = "#{ENV['SEED_DATA_SOURCE_URL']}characters.json"
-puts "seed #{uri}"
-open(uri) do |file|
-  characters = JSON.parse(file.read)
-  characters.each do |character|
-    Character.create!(character)
+unless Character.any?
+  uri = "#{ENV['SEED_DATA_SOURCE_URL']}characters.json"
+  puts "seed #{uri}"
+  open(uri) do |file|
+    characters = JSON.parse(file.read)
+    characters.each do |character|
+      Character.create!(character)
+    end
   end
 end
 
 # Team
-uri = "#{ENV['SEED_DATA_SOURCE_URL']}teams.json"
-puts "seed #{uri}"
-open(uri) do |file|
-  teams = JSON.parse(file.read)
-  teams.each do |team|
-    Team.create!(team)
+unless Team.any?
+  uri = "#{ENV['SEED_DATA_SOURCE_URL']}teams.json"
+  puts "seed #{uri}"
+  open(uri) do |file|
+    teams = JSON.parse(file.read)
+    teams.each do |team|
+      Team.create!(team)
+    end
   end
 end
 
 # Player
-uri = "#{ENV['SEED_DATA_SOURCE_URL']}players.json"
-puts "seed #{uri}"
-open(uri) do |file|
-  players = JSON.parse(file.read)
-  creator = DiscordUser.first
-  players.each do |player|
-    Player.create!(
-      name: player['name'],
-      city: player['city'].presence && City.find_by!(name: player['city']),
-      team: player['team'].presence && Team.find_by!(short_name: player['team']),
-      creator: creator
-      character_ids: player['characters'].map{|c| Character.find_by!(name: c).id}
-    )
+unless Player.any?
+  uri = "#{ENV['SEED_DATA_SOURCE_URL']}players.json"
+  puts "seed #{uri}"
+  open(uri) do |file|
+    players = JSON.parse(file.read)
+    creator = DiscordUser.first
+    players.each do |player|
+      Player.create!(
+        name: player['name'],
+        city: player['city'].presence && City.find_by!(name: player['city']),
+        team: player['team'].presence && Team.find_by!(short_name: player['team']),
+        creator: creator,
+        character_ids: player['characters'].map{|c| Character.find_by!(name: c).id}
+      )
+    end
   end
 end
 
