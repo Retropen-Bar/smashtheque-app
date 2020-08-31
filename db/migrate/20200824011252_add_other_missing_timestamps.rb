bfac21600111a732c96d30c79be1625b1e4e0a3d
Yann Hourdel <yann@hourdel.fr>
2020-08-31 02:49:09 +0200

    fix tests for CI

--- db/migrate/20200824011252_add_other_missing_timestamps.rb
+++ db/migrate/20200824011252_add_other_missing_timestamps.rb
@@ -1,13 +1,8 @@
 class AddOtherMissingTimestamps < ActiveRecord::Migration[6.0]
   def change
-    [Character, City].each do |klass|
-      t = klass.table_name
+    [:characters, :cities].each do |t|
       add_column t, :created_at, :datetime
       add_column t, :updated_at, :datetime
-      klass.update_all(
-        created_at: Time.now,
-        updated_at: Time.now
-      )
       change_column t, :created_at, :datetime, null: false
       change_column t, :updated_at, :datetime, null: false
     end
