374f01b83858a65fc00b0708b8b896c9b74f9254
Yann Hourdel <yann@hourdel.fr>
2020-08-31 21:05:39 +0200

    make public website public again

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,24 +1,8 @@
 class PublicController < ApplicationController
 
-  before_action :check_access!, except: :rollbar_test
-
   def home
     @players_count = Player.count
     @teams_count = Team.count
-  end
-
-  def rollbar_test
-    raise "This is a test"
-  end
-
-  private
-
-  def check_access!
-    # for the first time
-    session[:token] = params[:token] if params[:token]
-
-    # check
-    authenticate_admin_user! unless session[:token] == ENV['PUBLIC_ACCESS_TOKEN']
   end
 
 end
