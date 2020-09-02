c6eb5265050f9e6afdddee0c54cb4e0ae6f81fc1
Yann Hourdel <yann@hourdel.fr>
2020-09-02 02:44:37 +0200

    allow to hide website

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,8 +1,20 @@
 class PublicController < ApplicationController
 
+  before_action :check_access! if ENV['HIDE_WEBSITE']
+
   def home
     @players_count = Player.count
     @teams_count = Team.count
+  end
+
+  private
+
+  def check_access!
+    # for the first time
+    session[:token] = params[:token] if params[:token]
+
+    # check
+    authenticate_admin_user! unless session[:token] == ENV['PUBLIC_ACCESS_TOKEN']
   end
 
 end
