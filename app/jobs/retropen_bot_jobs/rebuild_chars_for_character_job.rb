0fdbe882026b68e6eff657b26079c4802fca064b
Yann Hourdel <yann@hourdel.fr>
2020-09-03 01:47:13 +0200

    fix job typo

--- app/jobs/retropen_bot_jobs/rebuild_chars_for_character_job.rb
+++ app/jobs/retropen_bot_jobs/rebuild_chars_for_character_job.rb
@@ -2,7 +2,7 @@ class RetropenBotJobs::RebuildCharsForCharacterJob < ApplicationJob
   queue_as :chars
 
   def perform(character, chars_category1_id: nil, chars_category2_id: nil)
-    RetropenBot.default.rebuild_chars_for_characters(
+    RetropenBot.default.rebuild_chars_for_character(
       character,
       chars_category1_id: chars_category1_id,
       chars_category2_id: chars_category2_id
