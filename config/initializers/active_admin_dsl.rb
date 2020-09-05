822d9721f3032daecf8f0cbadb7afc120b9084ce
Yann Hourdel <yann@hourdel.fr>
2020-09-06 00:21:36 +0200

    fix small issues

--- config/initializers/active_admin_dsl.rb
+++ config/initializers/active_admin_dsl.rb
@@ -1,3 +1,13 @@
 require 'active_admin/paper_trail/dsl'
 
 ::ActiveAdmin::DSL.send :include, ActiveAdmin::PaperTrail::DSL
+
+ActiveAdmin::Devise::SessionsController.class_eval do
+  def root_path
+    '/'
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+end
