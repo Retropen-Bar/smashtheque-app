da694e4125c7e6c45f16fc02c4b72245118bc4f2
Yann Hourdel <yann@hourdel.fr>
2020-12-05 00:34:20 +0100

    improve guilds index

--- app/controllers/discord_guilds_controller.rb
+++ app/controllers/discord_guilds_controller.rb
@@ -4,7 +4,16 @@ class DiscordGuildsController < PublicController
   has_scope :by_related_id
 
   def index
-    @discord_guilds = apply_scopes(DiscordGuild.known).all
+    base = DiscordGuild.known
+    @character_discord_guilds = base.by_related_type(Character.to_s).to_a
+    @team_discord_guilds = base.by_related_type(Team.to_s).to_a
+    @location_discord_guilds = base.by_related_type(Location.to_s).to_a
+    ids = @character_discord_guilds.map(&:id) +
+          @team_discord_guilds.map(&:id) +
+          @location_discord_guilds.map(&:id)
+    @other_discord_guilds = base.where.not(id: ids).to_a
+
+    # @discord_guilds = apply_scopes(DiscordGuild.known).all
   end
 
   def show
