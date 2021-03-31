2b6f66d71ceb96b6b4d018072b4d6d22358ecbab
Yann Hourdel <yann@hourdel.fr>
2021-03-31 15:59:46 +0200

    clean code

--- app/helpers/active_admin/addresses_helper.rb
+++ app/helpers/active_admin/addresses_helper.rb
@@ -3,31 +3,43 @@ module ActiveAdmin::AddressesHelper
   def address_input(form, options = {})
     prefix = options.delete(:prefix) || ''
     (
-      form.input  "#{prefix}address".to_sym,
-                  as: :string,
-                  input_html: {
-                    data: {
-                      maps_autocomplete: {
-                        target: {
-                          latitude: "#address-#{prefix}latitude",
-                          longitude: "#address-#{prefix}longitude"
-                        }
-                      }.merge(options)
-                    }
-                  }
+      address_address_input(form, prefix, options)
     ) + (
-      form.input  "#{prefix}latitude".to_sym,
-                  as: :hidden,
-                  input_html: {
-                    id: "address-#{prefix}latitude"
-                  }
+      address_latitude_input(form, prefix)
     ) + (
-      form.input  "#{prefix}longitude".to_sym,
-                  as: :hidden,
-                  input_html: {
-                    id: "address-#{prefix}longitude"
-                  }
+      address_longitude_input(form, prefix)
     )
+  end
+
+  def address_address_input(form, prefix, options)
+    form.input  "#{prefix}address".to_sym,
+                as: :string,
+                input_html: {
+                  data: {
+                    maps_autocomplete: {
+                      target: {
+                        latitude: "#address-#{prefix}latitude",
+                        longitude: "#address-#{prefix}longitude"
+                      }
+                    }.merge(options)
+                  }
+                }
+  end
+
+  def address_latitude_input(form, prefix)
+    form.input  "#{prefix}latitude".to_sym,
+                as: :hidden,
+                input_html: {
+                  id: "address-#{prefix}latitude"
+                }
+  end
+
+  def address_longitude_input(form, prefix)
+    form.input  "#{prefix}longitude".to_sym,
+                as: :hidden,
+                input_html: {
+                  id: "address-#{prefix}longitude"
+                }
   end
 
 end
