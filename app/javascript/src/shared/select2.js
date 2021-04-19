15a8db8c5c9479c3bc349e6464d8722822b142c8
Yann Hourdel <yann@hourdel.fr>
2021-04-19 21:55:24 +0200

    hotfix select2 focus

--- app/javascript/src/shared/select2.js
+++ app/javascript/src/shared/select2.js
@@ -1,6 +1,20 @@
 import "select2"
 import "select2/dist/js/i18n/fr"
 
+/*
+ * Hacky fix for a bug in select2 with jQuery 3.6.0's new nested-focus "protection"
+ * see: https://github.com/select2/select2/issues/5993
+ * see: https://github.com/jquery/jquery/issues/4382
+ *
+ * TODO: Recheck with the select2 GH issue and remove once this is fixed on their side
+ */
+
+$(document).on('select2:open', () => {
+  $('.select2-search__field').last()[0].focus();
+});
+
+// GO
+
 const DATA_KEY = 'data-select2';
 
 let getSortableUl = function($select) {
