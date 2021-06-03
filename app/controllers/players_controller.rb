cbe407b2001a3f0ff4e9c0423aba3b6f6c84251f
Léa <lea.chaminadas@gmail.com>
2021-06-03 22:35:24 +0200

    Style mobile bottom nav

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -14,6 +14,7 @@ class PlayersController < PublicController
   def index
     @players = players Player
     @meta_title = 'Joueurs'
+    render layout: 'application_v2'
   end
 
   def community_index
