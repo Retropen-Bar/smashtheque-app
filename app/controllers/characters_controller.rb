0f6ec325a82b10f8e675ad36569dc1334844ff6a
Yann Hourdel <yann@hourdel.fr>
2021-07-09 00:16:26 +0200

    init new characters index

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,10 +1,9 @@
 class CharactersController < PublicController
-
   has_scope :by_emoji
 
   def index
     @characters = apply_scopes(Character.order("lower(name)")).all
     @meta_title = 'Personnages'
+    render layout: 'application_v2'
   end
-
 end
