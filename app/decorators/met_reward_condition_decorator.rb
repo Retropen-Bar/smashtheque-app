a9f23db3e705292a4385d785269de98102747318
Liombe <lea.chaminadas@gmail.com>
2021-08-19 00:49:54 +0200

    WIP

--- app/decorators/met_reward_condition_decorator.rb
+++ app/decorators/met_reward_condition_decorator.rb
@@ -2,4 +2,8 @@ class MetRewardConditionDecorator < BaseDecorator
   def reward_badge(options = {})
     reward_condition.decorate.reward_badge(options)
   end
+  
+  def points_count(size, options = {})
+    awarded.decorate.points_count(size, options)
+  end
 end
