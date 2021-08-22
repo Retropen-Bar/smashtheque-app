29172b68207a454e6ab7ff644af368768920d432
Liombe <lea.chaminadas@gmail.com>
2021-08-23 01:35:29 +0200

    WIP

--- app/javascript/src/application_v2/vue.js
+++ app/javascript/src/application_v2/vue.js
@@ -1,11 +1,15 @@
 import Vue from "vue/dist/vue.common";
+import { PalmaresFilters } from "./palmares-filters";
 
 new Vue({
   el: "#app",
   data() {
     return {
       mainNavVisible: false,
-      filtersVisible: false
+      filtersVisible: false,
     };
+  },
+  components: {
+    "palmares-filters": PalmaresFilters,
   },
 });
