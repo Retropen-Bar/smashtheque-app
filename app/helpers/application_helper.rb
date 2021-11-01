d7a18520576958a94a8978d6eea141faca8bfc58
Liombe <lea.chaminadas@gmail.com>
2021-11-01 16:21:18 +0100

    Improve theme colors handling + tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,4 +42,37 @@ module ApplicationHelper
   def url_with(changes)
     url_for(current_page_params.merge(changes))
   end
+
+  def hex_color_to_rgba(hex, opacity)
+    rgb = hex.match(/^#(..)(..)(..)$/).captures.map(&:hex)
+    "rgba(#{rgb.join(", ")}, #{opacity})"
+  end
+
+  def darken_color(hex_color, amount=0.4)
+    hex_color = hex_color.gsub('#','')
+    rgb = hex_color.scan(/../).map {|color| color.hex}
+    rgb[0] = (rgb[0].to_i * amount).round
+    rgb[1] = (rgb[1].to_i * amount).round
+    rgb[2] = (rgb[2].to_i * amount).round
+    "#%02x%02x%02x" % rgb
+  end
+    
+  # Amount should be a decimal between 0 and 1. Higher means lighter
+  def lighten_color(hex_color, amount=0.6)
+    hex_color = hex_color.gsub('#','')
+    rgb = hex_color.scan(/../).map {|color| color.hex}
+    rgb[0] = [(rgb[0].to_i + 255 * amount).round, 255].min
+    rgb[1] = [(rgb[1].to_i + 255 * amount).round, 255].min
+    rgb[2] = [(rgb[2].to_i + 255 * amount).round, 255].min
+    "#%02x%02x%02x" % rgb
+  end
+
+  def contrasting_text_color(hex_color)
+    color = hex_color.gsub('#','')
+    convert_to_brightness_value(color) > 382.5 ? darken_color(color) : lighten_color(color)
+  end
+  
+  def convert_to_brightness_value(hex_color)
+    (hex_color.scan(/../).map {|color| color.hex}).sum
+  end
 end
