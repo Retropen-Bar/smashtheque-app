b97595a20730f564e54c9e2d3b0da174cabbf352
Liombe <lea.chaminadas@gmail.com>
2021-11-27 13:32:08 +0100

    Add new logo + fixes before deployment

--- app/javascript/src/active_admin/layout.js
+++ app/javascript/src/active_admin/layout.js
@@ -1,10 +1,7 @@
-import logoUrl from '../../images/smashtheque-64.png'
+import logoUrl from "../../images/smashtheque-short-black.svg";
 
-let updateLayout = function() {
-  $('h1#site_title').css(
-    'background-image',
-    'url("' + logoUrl + '")'
-  );
+let updateLayout = function () {
+  $("h1#site_title").css("background-image", 'url("' + logoUrl + '")');
 };
 
 $(document).ready(updateLayout);
