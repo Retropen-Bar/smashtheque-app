75adf38901ee24229c24c8cf4c9ec3567743cb6e
Yann Hourdel <yann@hourdel.fr>
2021-11-29 20:40:47 +0100

    Restore team links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,26 +15,34 @@ module ApplicationHelper
     count == 1 ? noun : noun.pluralize
   end
 
-  def admin_edit_link(resource)
-    if admin_user_signed_in?
-      link_to fas_icon_tag('cog'), [:admin, resource], target: '_blank', class: 'admin-edit-link'
-    end
+  def admin_edit_link(resource, options = {})
+    return unless admin_user_signed_in?
+
+    classes = [
+      'admin-edit-link',
+      options.delete(:class)
+    ].join(' ')
+    text = options.delete(:label) || fas_icon_tag('cog')
+    link_to text,
+            [:admin, resource],
+            { target: '_blank', rel: :noopener, class: classes }.merge(options)
   end
 
   def current_class?(path_prefix)
     return ' active' if request.path.starts_with?(path_prefix)
+
     ''
   end
 
   def current_section?(section_name)
     case section_name
     when :players
-      return true if controller_name.match(/(players|teams|duos|communities|characters)/)
+      controller_name =~ /(players|teams|duos|communities|characters)/
     else
-      return false
+      false
     end
   end
-  
+
   def current_section_name
     return :players if current_section?(:players)
   end
@@ -57,7 +65,7 @@ module ApplicationHelper
     rgb[2] = (rgb[2].to_i * amount).round
     "#%02x%02x%02x" % rgb
   end
-    
+
   # Amount should be a decimal between 0 and 1. Higher means lighter
   def lighten_color(hex_color, amount=0.6)
     hex_color = hex_color.gsub('#','')
@@ -72,7 +80,7 @@ module ApplicationHelper
     color = hex_color.gsub('#','')
     convert_to_brightness_value(color) > 382.5 ? darken_color(color) : lighten_color(color)
   end
-  
+
   def convert_to_brightness_value(hex_color)
     (hex_color.scan(/../).map {|color| color.hex}).sum
   end
